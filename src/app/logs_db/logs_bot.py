e09d6756febab1bfec2a4eb5d842b4253eb98ce9
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-29 05:27:52 +0300

    Delete logs_bot.py

--- src/app/logs_db/logs_bot.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from ..handler import ViewLogsRequestHandler
-from ..loader import load_logs_view
-
-
-def view_logs(data: ViewLogsRequestHandler) -> dict:
-    _viewer = load_logs_view()
-    return _viewer.view_logs(data)
